68ed3cb65212623384ed4e7bac8f335a519080c2
Hygor Silva <hygorss20@gmail.com>
2023-07-03 20:19:03 -0300

    acrescentando dependencia do Junit5

---
+++ src/test/java/UserProfileServiceTest.java
@@ -0,0 +1,9 @@
+import br.com.openai.medical.aimedicalappointment.service.UserProfileService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserProfileServiceTest {
+
+ 
+
+}
