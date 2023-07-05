d97164a734fe07ba436aa838895c86c6b90dbabe
Emerson Rocha <emerson.rocha@ebanx.com>
2023-07-05 13:07:23 -0300

    Adjusts in unit tests

--- src/test/java/br/com/openai/medical/aimedicalappointment/AiMedicalAppointmentApplicationTests.java
+++ src/test/java/br/com/openai/medical/aimedicalappointment/AiMedicalAppointmentApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.openai.medical.aimedicalappointment;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AiMedicalAppointmentApplicationTests {
 
   @Test
